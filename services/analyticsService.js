f79874c265a6b40a033c46fbe633390df5686fba
Vesa Suikkola <vesa.suikkola@gmail.com>
2021-02-19 14:37:00 +0200

    Fix in api port env var code

--- services/analyticsService.js
+++ services/analyticsService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_PATHS } from '../config.js';
 
 const API_HOST = process.env.API_HOST || 'localhost';
-const API_PORT = process.env.API_POST || 4000;
+const API_PORT = process.env.API_PORT || 4000;
 const API = `${API_HOST}:${API_PORT}/${API_PATHS.analytics_db.views}`;
 
 export const updateViews = async (code, jwt) =>
